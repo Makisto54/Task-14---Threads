b88f28fef33e81ad141e3d1e14de12782e3330e8
Makisto54 <ilja-martasov@mail.ru>
2021-08-13 16:29:40 +0700

    Fix 0 shop

--- thread_shop/src/client.c
+++ thread_shop/src/client.c
@@ -8,27 +8,30 @@ void *client(void *args)
     while (client->cost != 0)
     {
         i = rand () % 5;
-        if (!pthread_mutex_trylock(&mutex[i]))
+        if (0 != shop[i])
         {
-            printf("\n\nCLIENT %d WORKING\n\n", client->id);
-            printf("CLIENT %d CURRENT COST - %d\nSHOP %d CURRENT COST - %d\n", 
-                client->id, client->cost, i, shop[i]);
-            if (client->cost < shop[i])
+            if (!pthread_mutex_trylock(&mutex[i]))
             {
-                shop[i] -= client->cost;
-                client->cost = 0;
-            }
-            else
-            {
-                client->cost -= shop[i]; 
-                shop[i] -= shop[i];          
-            }
-            printf("CLIENT %d AFTER BUYING - CURRENT COST - %d SHOP %d COST - %d\n", 
-                client->id, client->cost, i, shop[i]);
-            printf("\nCLIENT %d END WORKING\n\n", client->id);
-            pthread_mutex_unlock(&mutex[i]);
+                printf("\n\nCLIENT %d WORKING\n\n", client->id);
+                printf("CLIENT %d CURRENT COST - %d\nSHOP %d CURRENT COST - %d\n", 
+                    client->id, client->cost, i, shop[i]);
+                if (client->cost < shop[i])
+                {
+                    shop[i] -= client->cost;
+                    client->cost = 0;
+                }
+                else
+                {
+                    client->cost -= shop[i]; 
+                    shop[i] -= shop[i];          
+                }
+                printf("CLIENT %d AFTER BUYING - CURRENT COST - %d SHOP %d COST - %d\n", 
+                    client->id, client->cost, i, shop[i]);
+                printf("\nCLIENT %d END WORKING\n\n", client->id);
+                pthread_mutex_unlock(&mutex[i]);
+            }  
+            sleep(2);         
         }
-        sleep(2);
     }
  
     printf("CLIENT %d END HIS WORK. HIS COST - %d\n", client->id, client->cost);
